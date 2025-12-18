9cd58bc2c0c512559e2b1c07caaf6a23c8cb5f89
markeed-code <66025059+markeed-code@users.noreply.github.com>
2025-12-18 14:33:43 +0000

    Add files via upload

---
+++ addExternal_curl.cmake
@@ -0,0 +1,295 @@
+#[=============================================================================[
+ *|            This source code is provided under the Apache 2.0 license
+ *|  and is provided AS IS with no warranty or guarantee of fit for purpose.
+ *|                See the project's LICENSE.md for details.
+ *|           Copyright (C) 2019,2021-2025 LSEG. All rights reserved.
+#]=============================================================================]
+
+include(rcdevExternalUtils)
+
+if(NOT curl_url)
+	set(curl_url "https://github.com/curl/curl/releases/download/curl-8_12_1/curl-8.12.1.tar.xz")
+endif()
+if(NOT curl_hash)
+	set(curl_hash "MD5=7940975dd510399c4b27831165ab62e0")
+endif()
+if(NOT curl_version)
+	set(curl_version "8.12.1")
+endif()
+
+# If the option for using the system installed 
+#  package is not defined
+if((NOT curl_USE_INSTALLED) AND 
+	(NOT TARGET CURL::libcurl) )
+	set(_EPA_NAME "curl")
+
+	# Initialize the directory variables for the external project
+	# default:
+	#        external/
+	#                dlcache/
+	#                  BUILD/_EP_NAME/
+	#                               source/
+	#                               build/
+	#        install/
+	rcdev_init_ep_add(${_EPA_NAME})
+
+	# get the file name off the url to ensure it is
+	# downloaded with the same name
+	get_filename_component(_dl_filename "${curl_url}" NAME)
+	set(_DL_METHOD	"URL           ${curl_url}" )
+
+	if(curl_hash)
+		list(APPEND _DL_METHOD "URL_HASH       ${curl_hash}")
+	endif()
+
+	list(APPEND _DL_METHOD "DOWNLOAD_DIR  ${curl_download}")
+
+	if (DEFINED _dl_filename)
+		list(APPEND _DL_METHOD "DOWNLOAD_NAME ${_dl_filename}" )
+	endif()
+
+	set(_EPA_SOURCE_DIR "SOURCE_DIR ${curl_source}")
+	# the BINARY_DIR is not seperate for this type of external project
+	set(_EPA_INSTALL_DIR "INSTALL_DIR ${curl_install}")
+
+	# check for any defined flags
+	# The shared library is the default build
+	if(curl_BUILD_STATIC_LIBS)
+		set(_shared_arg "-DBUILD_SHARED_LIBS:BOOL=OFF")
+	else()
+		set(_shared_arg "-DBUILD_SHARED_LIBS:BOOL=ON")
+	endif()
+
+	# The curl distribution has a autoconf configure method a well as a CMake.
+	# These _CONFIG_OPTIONS would be used if this external add was changed to build curl
+	# using the autouconf method
+	#[=============================================================[
+	if(curl_CONFIG_OPTIONS)
+		set(_config_options "${curl_CONFIG_OPTIONS}")
+	else()
+		set(_config_options "no-asm no-tests no-comp no-srp no-hw no-dso ")
+	endif()
+	#]=============================================================]
+
+	if(curl_CONFIG_OPTIONS)
+		set(_config_options "${curl_CONFIG_OPTIONS}")
+	else()
+		# append the standard set of cmake configuration args
+		set(_config_options	"-DBUILD_CURL_EXE:BOOL=OFF"
+						"-DCMAKE_USE_LIBSSH2:BOOL=OFF"
+						"-DHTTP_ONLY:BOOL=ON"
+						"-DBUILD_TESTING:BOOL=OFF"
+						"-DENABLE_IPV6:BOOL=OFF"
+						"-DENABLE_MANUAL:BOOL=OFF"
+						"-DCURL_CA_PATH=none"
+						"-DBUILD_LIBCURL_DOCS=OFF"
+						"-DBUILD_MISC_DOCS=OFF"
+						"-DENABLE_CURL_MANUAL=OFF"
+						"-DCURL_USE_LIBPSL=OFF"
+						)
+	endif()
+
+	unset(_cfg_type)
+	if(WIN32)
+		set(_config_options "${_config_options}"
+						"-DCURL_USE_SCHANNEL:BOOL=ON"
+						"-DCMAKE_POLICY_DEFAULT_CMP0091:STRING=NEW"
+						"-DCMAKE_MSVC_RUNTIME_LIBRARY:STRING=${CMAKE_MSVC_RUNTIME_LIBRARY_FOR_EXTERNAL}"
+						"-DCURL_STATIC_CRT:BOOL=ON")
+#						"-DCMAKE_USE_WINSSL:BOOL=ON")
+	else()
+		set(_config_options "${_config_options}" 
+							"-DCURL_USE_OPENSSL:BOOL=ON")
+
+		# Need to search for the correct OpenSsl version incase there are 64 and 32 bit 
+		# versions installed, since the curl build and install will not protect itself from 
+		# linking with the incorrect arch version
+		find_package(PkgConfig QUIET)
+		if (NOT CMAKE_PREFIX_PATH)
+			set(CMAKE_PREFIX_PATH "/usr" "/usr/local")
+		endif()
+		string(REPLACE ";" "|" CMAKE_PREFIX_PATH_TMP "${CMAKE_PREFIX_PATH}")
+		PKG_CHECK_MODULES(_OPENSSL QUIET openssl)
+
+		DEBUG_PRINT(::ALL _OPENSSL)
+
+		if (NOT ("x${_OPENSSL_LIBDIR}x" STREQUAL "xx"))
+			list(APPEND _config_options "-DOpenSSL_DIR:PATH=${_OPENSSL_LIBDIR}")
+		endif()
+
+		# Since our internal build types are Debug and Optimized, only Debug will translate
+		if (CMAKE_BUILD_TYPE MATCHES "Debug")
+			set(_cfg_type "${CMAKE_BUILD_TYPE}")
+			list(APPEND _config_options "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"
+										"-DCMAKE_DEBUG_POSTFIX:STRING= ")
+		else()
+			set(_cfg_type "Release")
+			list(APPEND _config_options "-DCMAKE_BUILD_TYPE:STRING=Release")
+		endif()
+
+		set(_libdir "lib")
+		if (RCDEV_HOST_SYSTEM_BITS STREQUAL "64")
+			set(_libdir "lib64")
+		endif()
+
+		list(APPEND _config_options "-DCMAKE_C_FLAGS:STRING=-m${RCDEV_HOST_SYSTEM_BITS}"
+									"-DCMAKE_CXX_FLAGS:STRING=-m${RCDEV_HOST_SYSTEM_BITS}"
+									"-DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH_TMP}"
+									"-DCMAKE_SIZEOF_VOID_P=${CMAKE_SIZEOF_VOID_P}"
+									"-DLIBDIR:STRING=${_libdir}"
+									)
+
+	endif()
+
+	if(curl_BUILD_STATIC_LIBS)
+		set(_config_options "${_config_options}"
+						"-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON")
+	endif()
+	# Append the config and shared args to the CMake arguments to the template variable
+	set( _EPA_CMAKE_ARGS "CMAKE_ARGS"
+						"-DCMAKE_INSTALL_PREFIX:STRING=<INSTALL_DIR>"
+						"${_config_options}"
+						"${_shared_arg}"
+						"LIST_SEPARATOR |"
+						)
+
+	# Since this external project has a CMakeLists.txt, the default CONFIG_COMMAND will be
+	# used and one sonce not need to be defined here.
+	#  Adding CONFIGURE_COMMAND  "" would skip the default CMake configure step
+	#  list(APPEND _EPA_CONFIGURE_COMMAND  "CONFIGURE_COMMAND  \"\"" )
+
+	# Typically, the build and install steps can be combined.  However, having them as 
+	#  two seperate steps help in the event of having to debug a build
+	# Set the <.....>_COMMAND for the build and install template fields
+	# However, for this external project it is works out better to combine these next two steps
+	# within the INSTALL_COMMAND step.  So, this is skipping the BUILD_COMMAND by 
+	# passing "" as the argument for the BUILD_COMMAND
+	if (WIN32)
+		set( _EPA_BUILD_COMMAND 
+					"BUILD_COMMAND     \"${CMAKE_COMMAND}\"   --build .  --config Release "  
+					"COMMAND    \"${CMAKE_COMMAND}\"   --build .  --config Debug ")
+	else()
+		set( _EPA_BUILD_COMMAND 
+					"BUILD_COMMAND    ${CMAKE_COMMAND}   --build .  --config ${_cfg_type} ")
+	endif()
+
+	# Passing the two supported build config types along to the INSTALL_COMMAND for Windows and for 
+	# single build type platforms, like Linux, the current config typed is built and installed
+	if (WIN32)
+		set( _EPA_INSTALL_COMMAND 
+					"INSTALL_COMMAND    \"${CMAKE_COMMAND}\"   --build .  --target install  --config Release "
+  					"	COMMAND    \"${CMAKE_COMMAND}\"   --build .  --target install  --config Debug ")
+	else()
+		set( _EPA_INSTALL_COMMAND 
+					"INSTALL_COMMAND    ${CMAKE_COMMAND}   --build .  --target install  --config ${_cfg_type} ")
+	endif()	
+
+	set(_EPA_TEST_COMMAND "TEST_COMMAND \"\"")
+
+	# If there isn't a binary directory defined then make sure
+	# the option 'BUILD_IN_SOURCE' is enabled
+	if (NOT DEFINED _EPA_BINARY_DIR)
+		set(_EPA_ADDITIONAL_ARGS "BUILD_IN_SOURCE 1" )
+	endif()
+
+	# Add log defiitions if selected to be enabled and append them to the
+	# additional args variable
+	if(curl_LOG_BUILD)
+		set(_log_args 
+						"LOG_CONFIGURE 1"
+						"LOG_BUILD 1"
+						"LOG_INSTALL 1"
+			)
+	endif()
+
+	list(APPEND _EPA_ADDITIONAL_ARGS "${_log_args}")
+
+	# Call cmake configure and build on the CMakeLists.txt file
+	# written using the previously set template arguments
+	rcdev_config_build_ep(${_EPA_NAME})
+
+	# this policy is needed to supress a CMake warning about the new
+	# standard for using <project>_ROOT variable for find_package()
+	if( POLICY CMP0074 )
+		#message("Setting CMake policy CMP0074 ${_EPA_NAME}:[ ${CMAKE_CURRENT_LIST_FILE}:${CMAKE_CURRENT_LIST_LINE} ] ")
+		cmake_policy(SET CMP0074 NEW)
+	endif()
+
+	# Not yet supported by the Curl Find module, but set it for good practice
+	if(NOT CURL_ROOT)
+		set(CURL_ROOT "${curl_install}" CACHE INTERNAL "")
+	endif()
+
+	unset(_libdir)
+	unset(_shared_arg)
+	unset(_config_args)
+	unset(_log_args)
+	unset(_dl_filename)
+
+	# This call will reset all the _EPA_... variables. Because this is a
+	# macro and if this is not called, the next external project using
+	# this template will be at risk being currupted with old values.
+	rcdev_reset_ep_add()
+	
+	set(curl_find_options PATHS ${curl_install} NO_DEFAULT_PATH)
+
+endif()
+
+# Find the package, for both a system installed version or the one
+# just added with the ecternal project template
+if ((NOT CURL_FOUND) OR
+	(NOT TARGET CURL::libcurl))
+	# Calling find_package with a required version number will fail if the
+	# package does not have a <name>version.cmake in the same location as
+	# the <package>config.cmake.  Unfortunately, CMake will not use the version
+	# field defiition within a <package>.pc file. Also, the option to search for the
+	# newly built version are passed as an argument, in case they have been defined, 
+	# in lieu of an installed version
+	find_package(CURL REQUIRED ${curl_find_options})
+
+	# This condition is here since the FindCURL CMake module for version < Cmake.12.0 does not 
+	#  crete an IMPORTED targte object (CURL::libcurl)
+	if(NOT TARGET CURL::libcurl)
+		add_library(CURL::libcurl UNKNOWN IMPORTED)
+		set_target_properties(CURL::libcurl PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
+		set_property(TARGET CURL::libcurl APPEND PROPERTY IMPORTED_LOCATION "${CURL_LIBRARY}")
+	endif()
+
+	if(WIN32)
+		get_property(aliased_target TARGET CURL::libcurl PROPERTY ALIASED_TARGET)
+		if("${aliased_target}" STREQUAL "")
+			# is not an alias
+			rcdev_map_imported_ep_types(CURL::libcurl)
+		else()
+			# is an alias
+			rcdev_map_imported_ep_types(${aliased_target})
+		endif()
+	endif()
+
+	if( (DEFINED CURL_VERSION_STRING) AND
+		(CURL_VERSION_STRING VERSION_LESS "${curl_version}") )
+		message(WARNING
+				"  libcurl ver:${CURL_VERSION_STRING} found, is older than the supported ver:${curl_version}\n"
+				"  This may cause unexpected behavior and/or build results"
+				)
+	# In case the version string from the package config is defined
+	elseif( (DEFINED CVF_VERSION_MAJOR) AND
+		(CVF_VERSION_MAJOR VERSION_LESS "${curl_version}") )
+		message(WARNING
+				"  libcurl ver:${CVF_VERSION_MAJOR } found, is older than the supported ver:${curl_version}\n"
+				"  This may cause unexpected behavior and/or build results"
+				)
+	endif()
+
+	rcdev_add_external_target(CURL::libcurl)
+
+endif()
+
+DEBUG_PRINT(OpenSSL::SSL)
+DEBUG_PRINT(OpenSSL::Crypto)
+DEBUG_PRINT(CURL_FOUND)
+DEBUG_PRINT(CURL_LIBRARY)
+DEBUG_PRINT(CURL_VERSION_STRING)
+DEBUG_PRINT(CURL::libcurl)
+
+
---
+++ rcdevCompilerOptions.cmake
@@ -0,0 +1,245 @@
+#[=============================================================================[
+ *|            This source code is provided under the Apache 2.0 license
+ *|  and is provided AS IS with no warranty or guarantee of fit for purpose.
+ *|                See the project's LICENSE.md for details.
+ *|           Copyright (C) 2019-2021,2024-2025 LSEG. All rights reserved.
+#]=============================================================================]
+
+# build flags
+# NOTES FOR TODO:
+# For UNIX
+#      PARTIALLY DONE with rcdev_update_initial_cache()!
+#	1. Set all compiler options/flags for each build type
+#         e.g.  _DEBUG, _OPTIMIZED, _OPTIMIZEDDEBUG
+#			 CMAKE_CXX_FLAGS_DEBUG=-g
+#			 CMAKE_CXX_FLAGS_DEBUG_INIT= -g
+#			 CMAKE_C_FLAGS_DEBUG=-g
+#			 CMAKE_C_FLAGS_DEBUG_INIT= -g
+#			 CMAKE_EXE_LINKER_FLAGS_DEBUG=
+#			 CMAKE_MODULE_LINKER_FLAGS_DEBUG=
+#			 CMAKE_SHARED_LINKER_FLAGS_DEBUG=
+#			 CMAKE_STATIC_LINKER_FLAGS_DEBUG=
+#
+#      DONE!
+#   2. Loop through the build types (DEBUG OPTIMIZED OPTIMIZEDDEBUG RELEASE MINSIZEREL RELWITHDEBINFO)
+#      If the current build type is != CMAKE_BUILD_TYPE clear the CACHE entry
+#					unset(vars above CACHE)
+#         
+#   3. Then for the current CMAKE_BUILD_TYPE set the values to CACHE
+#     i.e.
+#			set_property(CACHE CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_U} ${CMAKE_CXX_FLAGS_DEBUG}
+#
+
+if (NOT _def_rcdev_add_if_not_set)
+set(_def_rcdev_add_if_not_set 1)
+function(rcdev_add_if_not_set _outstring _flags _xtra_flags)
+	# Break up the two lists into CMake lists
+	if (WIN32)
+		set(_delChar "/")
+	else()
+		set(_delChar "-")
+	endif()
+
+	# create two lists for current flags and xtra flags to be added
+	string(REPLACE " ${_delChar}" ";${_delChar}" _flList "${_flags}")
+	string(REPLACE " ${_delChar}" ";${_delChar}" _xflList "${_xtra_flags}")
+
+	unset(_append)
+	foreach( _xflg ${_xflList})
+		unset(_found_flag)
+		# Check each flag in flList if this xtra flag exists
+		# message("Checking xtra flag '${_xflg}':")
+		foreach( _flg ${_flList})
+
+			if("${_xflg}" STREQUAL "${_flg}")
+				# If the xtra flag exists, set a found flag
+				# and break to process the next xtra flag
+				#message("\t'${_xflg}' ='s '${_flg}'")
+				set(_found_flag 1)
+				break()
+			endif()
+		endforeach()
+
+		# If the xtra flag was not in the flList then
+		# append it to a new local list
+		if (NOT _found_flag)
+			string(APPEND _append " ${_xflg}")
+		endif()
+	endforeach()
+
+	# If there are new flags from xtra list to add,
+	# then append the new local list and set it to  
+	# the parent scope
+	if (NOT ("x${_append}x" STREQUAL "xx"))
+		set(${_outstring} "${_flags}${_append}" PARENT_SCOPE)
+	endif()
+
+endfunction()
+endif()
+
+macro(rcdev_set_flags _flags _xtra_flags)
+
+	rcdev_add_if_not_set(_rcdev_output ${${_flags}} ${${_xtra_flags}})
+
+	if (NOT ("x${_rcdev_output}x" STREQUAL "xx"))
+		set(${_flags} "${_rcdev_output}" CACHE STRING "" FORCE)
+		unset(_rcdev_output)
+	endif()
+
+endmacro()
+
+if( UNIX )
+
+	if(${RCDEV_HOST_SYSTEM_BITS} STREQUAL "64")
+        set(_compilerBitFlags "-m64 -DCOMPILE_64BITS -fPIC")
+    else()
+        set(_compilerBitFlags "-m32")
+    endif()
+
+	# flags for C
+    set( RCDEV_C_FLAGS_INIT "${_compilerBitFlags} -D_DEFAULT_SOURCE=1  -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -pthread -D_iso_stdcpp_ -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE" CACHE STRING "" FORCE )
+
+    # Suppress Clang formatting warnings only
+    if ( CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
+        set ( CMAKE_FLAGS_CLANG "-fno-omit-frame-pointer -Wno-format -Wno-format-security -Wno-parentheses -Wno-empty-body -Wno-unused-value -Wno-comment -Wno-logical-op-parentheses -Wno-dangling-else -Wno-constant-logical-operand -Wno-macro-redefined  -Wno-pointer-sign -Wno-constant-conversion -Wno-switch -Wno-enum-conversion -Wno-sizeof-pointer-memaccess -Wno-null-dereference -Wno-fortify-source -Wno-undefined-bool-conversion" CACHE STRING ""  FORCE )
+    endif()
+
+    if(DEFINED BUILD_CODE_COVERAGE)
+        set ( CMAKE_CODE_COVERAGE_FLAGS "-fprofile-arcs -ftest-coverage -O0 -g" )
+    endif()
+
+	if ( ${CMAKE_BUILD_TYPE} STREQUAL "Optimized" )
+		set ( CMAKE_C_FLAGS "${RCDEV_C_FLAGS_INIT} -DNDEBUG -O3 -finline-limit=700 -fbuiltin ${CMAKE_FLAGS_CLANG}" CACHE STRING "" FORCE)
+	elseif ( ${CMAKE_BUILD_TYPE} STREQUAL "Debug" )
+		set ( CMAKE_C_FLAGS "${RCDEV_C_FLAGS_INIT} -ggdb3 ${CMAKE_FLAGS_CLANG} ${CMAKE_CODE_COVERAGE_FLAGS}" CACHE STRING "" FORCE)
+	elseif ( ${CMAKE_BUILD_TYPE} STREQUAL "OptimizedDebug" )
+		set ( CMAKE_C_FLAGS "${RCDEV_C_FLAGS_INIT} -ggdb3 -DNDEBUG -O3 -finline-limit=700 -fbuiltin ${CMAKE_FLAGS_CLANG}" CACHE STRING "" FORCE)
+	endif()
+
+	if (RCDEV_C_EXTRA_FLAGS)
+		rcdev_set_flags(CMAKE_C_FLAGS RCDEV_C_EXTRA_FLAGS)
+	endif()
+
+	# flags for C++
+    set( RCDEV_CXX_FLAGS_INIT "${_compilerBitFlags} -DLinux -DLINUX -Dx86_Linux_4X -Dx86_Linux_5X -Dx86_Linux_6X -DLinuxVersion=${RCDEV_HOST_SYSTEM_FLAVOR_REL} -Wno-ctor-dtor-privacy -Wno-deprecated -std=c++11 -pthread  -D_iso_stdcpp_ -D_DEFAULT_SOURCE=1 -D_POSIX_SOURCE=1 -D_POSIX_C_SOURCE=199506L -D_XOPEN_SOURCE=500 -D_GNU_SOURCE"  CACHE STRING "" FORCE)
+	if ( ${CMAKE_BUILD_TYPE} STREQUAL "Optimized" )
+		set ( CMAKE_CXX_FLAGS "${RCDEV_CXX_FLAGS_INIT} -DNDEBUG -O3 -fbuiltin ${CMAKE_FLAGS_CLANG}" CACHE STRING ""  FORCE)
+	elseif ( ${CMAKE_BUILD_TYPE} STREQUAL "Debug" )
+		set ( CMAKE_CXX_FLAGS "${RCDEV_CXX_FLAGS_INIT} -ggdb3 ${CMAKE_FLAGS_CLANG} ${CMAKE_CODE_COVERAGE_FLAGS}"  CACHE STRING "" FORCE)
+	elseif ( ${CMAKE_BUILD_TYPE} STREQUAL "OptimizedDebug" )
+		set ( CMAKE_CXX_FLAGS "${RCDEV_CXX_FLAGS_INIT} -ggdb3 -DNDEBUG -O3 -fbuiltin ${CMAKE_FLAGS_CLANG}"  CACHE STRING "" FORCE)
+	endif()
+
+	if (RCDEV_CXX_EXTRA_FLAGS)
+		rcdev_set_flags(CMAKE_CXX_FLAGS RCDEV_CXX_EXTRA_FLAGS)
+	endif()
+
+    if( ${RCDEV_HOST_SYSTEM_BITS} STREQUAL "32")
+		set(CMAKE_POSITION_INDEPENDENT_CODE 0)
+		set(CMAKE_SHARED_LINKER_FLAGS "${_compilerBitFlags}" CACHE STRING "" FORCE)
+		set(CMAKE_EXE_LINKER_FLAGS "${_compilerBitFlags}" CACHE STRING "" FORCE)
+    endif()
+
+else()
+
+	# Enable PDB files for Release_MD
+	if (RCDEV_ENABLE_PDB_FILES)
+		set(_pdb_compile_flags "/Zi")
+		set(_pdb_linker_flags "/DEBUG /OPT:REF /OPT:ICF")
+	endif()
+
+	#Static library debugging symbols setting
+	set(RCDEV_DEBUG_TYPE_FLAGS_STATIC "/Z7")
+
+	#Non-static library debugging symbols settings
+	set(RCDEV_DEBUG_TYPE_FLAGS_NONSTATIC "/Zi")
+	set(RCDEV_TYPE_CHECK_FLAG "/GR")
+	
+	if(${RCDEV_HOST_SYSTEM_BITS} STREQUAL "64")
+		set(RCDEV_COMPILE_BITS "/D COMPILE_64BITS")
+	endif()
+	
+	#Release flags for non-static projects 
+	set(RCDEV_FLAGS_NONSTATIC_RELEASE "/GL")
+
+	set ( RCDEV_C_EXTRA_FLAGS "-D_DEBUG_WS_CONN" )
+
+	#CMake 3.15 CMP0091 rule for selecting MSVC runtime
+	# Map both the repo-specific configuration names and the common Debug/Release
+	# so external projects (which may use Debug/Release) inherit the static CRT.
+	set(CMAKE_MSVC_RUNTIME_LIBRARY "$<$<CONFIG:Debug_Mdd>:MultiThreadedDebug>$<$<CONFIG:Debug>:MultiThreadedDebug>$<$<CONFIG:Release_MD>:MultiThreaded>$<$<CONFIG:Release>:MultiThreaded>" CACHE STRING "" FORCE)
+	
+	# For external projects: resolve CMAKE_MSVC_RUNTIME_LIBRARY to a concrete value
+	# based on CMAKE_BUILD_TYPE or CMAKE_CONFIGURATION_TYPES (if set at config time)
+	if(CMAKE_BUILD_TYPE)
+		if(CMAKE_BUILD_TYPE STREQUAL "Debug_Mdd" OR CMAKE_BUILD_TYPE STREQUAL "Debug")
+			set(CMAKE_MSVC_RUNTIME_LIBRARY_FOR_EXTERNAL "MultiThreadedDebug")
+		else()
+			set(CMAKE_MSVC_RUNTIME_LIBRARY_FOR_EXTERNAL "MultiThreaded")
+		endif()
+	else()
+		# Multi-config generator: default to Release/MultiThreaded unless we detect Debug
+		set(CMAKE_MSVC_RUNTIME_LIBRARY_FOR_EXTERNAL "MultiThreaded")
+	endif()
+
+	# flags for C
+	set(RCDEV_C_FLAGS_INIT "/GS /W3 /Zc:wchar_t- /fp:precise /fp:except- /Gm- /D WIN32 /D _WINDOWS /D _WIN32 /D WIN32_LEAN_AND_MEAN /D _WIN32_WINNT=_WIN32_WINNT_VISTA /D x86_WindowsNT_5X /D _CRT_SECURE_NO_WARNINGS ${RCDEV_COMPILE_BITS} /D _iso_stdcpp_ /errorReport:prompt /WX- /Gd /Zc:forScope /EHsc /nologo /Oi " CACHE STRING "" FORCE)
+	set(CMAKE_C_FLAGS_DEBUG_MDD "${RCDEV_C_FLAGS_INIT} /Gy- /Od /RTC1 /MTd /GF- ${_pdb_compile_flags} " CACHE STRING "" FORCE)
+	set(CMAKE_C_FLAGS_RELEASE_MD "${RCDEV_C_FLAGS_INIT} /O2 /Ob2 /Ot /MT /Gy /GF /D NDEBUG ${_pdb_compile_flags} " CACHE STRING "" FORCE)
+
+	# Also provide the standard Debug/Release variables so external/third-party
+	# CMake projects that use the conventional config names get /MT(/MTd).
+	set(CMAKE_C_FLAGS_DEBUG "${RCDEV_C_FLAGS_INIT} /Gy- /Od /RTC1 /MTd /GF- ${_pdb_compile_flags} " CACHE STRING "" FORCE)
+	set(CMAKE_C_FLAGS_RELEASE "${RCDEV_C_FLAGS_INIT} /O2 /Ob2 /Ot /MT /Gy /GF /D NDEBUG ${_pdb_compile_flags} " CACHE STRING "" FORCE)
+
+	if (RCDEV_C_EXTRA_FLAGS)
+		rcdev_set_flags(CMAKE_C_FLAGS RCDEV_C_EXTRA_FLAGS)
+	endif()
+	if (RCDEV_C_EXTRA_FLAGS_DEBUG_MDD)
+		rcdev_set_flags(CMAKE_C_FLAGS_DEBUG_MDD RCDEV_C_EXTRA_FLAGS_DEBUG_MDD)
+	endif()
+	if (RCDEV_C_EXTRA_FLAGS_RELEASE_MD)
+		rcdev_set_flags(CMAKE_C_FLAGS_RELEASE_MD RCDEV_C_EXTRA_FLAGS_RELEASE_MD)
+	endif()
+	
+	# CXX flags
+	set(RCDEV_CXX_FLAGS_INIT "/GS /W3 /Zc:wchar_t- /fp:precise /fp:except- /Gm- /D WIN32 /D _WINDOWS /D _WIN32 /D WIN32_LEAN_AND_MEAN /D _WIN32_WINNT=_WIN32_WINNT_VISTA /D x86_WindowsNT_5X /D _CRT_SECURE_NO_WARNINGS ${RCDEV_COMPILE_BITS} /D _iso_stdcpp_ /errorReport:prompt /WX- /Gd /Zc:forScope /EHsc /nologo /Oi " CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS_DEBUG_MDD "${RCDEV_CXX_FLAGS_INIT} /Gy- /Od /RTC1 /MTd /GF-" CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS_RELEASE_MD "${RCDEV_CXX_FLAGS_INIT} /O2 /Ob2 /Ot /MT /Gy /GF /D NDEBUG" CACHE STRING "" FORCE)
+
+	# Provide conventional Debug/Release variants for external projects.
+	set(CMAKE_CXX_FLAGS_DEBUG "${RCDEV_CXX_FLAGS_INIT} /Gy- /Od /RTC1 /MTd /GF-" CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS_RELEASE "${RCDEV_CXX_FLAGS_INIT} /O2 /Ob2 /Ot /MT /Gy /GF /D NDEBUG" CACHE STRING "" FORCE)
+	
+	if (RCDEV_CXX_EXTRA_FLAGS)
+		rcdev_set_flags(CMAKE_CXX_FLAGS RCDEV_CXX_EXTRA_FLAGS)
+	endif()
+	if (RCDEV_CXX_EXTRA_FLAGS_DEBUG_MDD)
+		rcdev_set_flags(CMAKE_CXX_FLAGS_DEBUG_MDD RCDEV_CXX_EXTRA_FLAGS_DEBUG_MDD)
+	endif()
+	if (RCDEV_CXX_EXTRA_FLAGS_RELEASE_MD)
+		rcdev_set_flags(CMAKE_CXX_FLAGS_RELEASE_MD RCDEV_CXX_EXTRA_FLAGS_RELEASE_MD)
+	endif()
+	
+	#Linker flags
+	# flags to be added when linking external project target objects
+	if(NOT RCDEV_EXTERNAL_LINK_FLAGS)
+		# To supress warning " : warning lnk4099: pdb 'xxxx.pdb' was not found with ..."
+		set(RCDEV_EXTERNAL_LINK_FLAGS "/ignore:4099")
+	endif()
+	
+	set(CMAKE_SHARED_LINKER_FLAGS_DEBUG_MDD "${CMAKE_SHARED_LINKER_FLAGS_INIT} /DEBUG /NODEFAULTLIB:MSVCRTD /INCREMENTAL:NO"  CACHE STRING "" FORCE)
+	set(CMAKE_SHARED_LINKER_FLAGS_RELEASE_MD "${CMAKE_SHARED_LINKER_FLAGS_INIT} /LTCG /INCREMENTAL:NO /NODEFAULTLIB:MSVCRTD ${_pdb_linker_flags}"  CACHE STRING "" FORCE)
+
+	set(CMAKE_EXE_LINKER_FLAGS_DEBUG_MDD "${CMAKE_EXE_LINKER_FLAGS_INIT} /DEBUG /NODEFAULTLIB:MSVCRTD /INCREMENTAL:NO"  CACHE STRING "")
+	set(CMAKE_EXE_LINKER_FLAGS_RELEASE_MD "${CMAKE_EXE_LINKER_FLAGS_INIT} /LTCG /INCREMENTAL:NO /NODEFAULTLIB:MSVCRTD ${_pdb_linker_flags}"  CACHE STRING "")
+  
+endif()
+
+DEBUG_PRINT(CMAKE_C_FLAGS)
+DEBUG_PRINT(CMAKE_CXX_FLAGS)
+
+unset(_compilerBitFlags)
+# end build flags
+
+
